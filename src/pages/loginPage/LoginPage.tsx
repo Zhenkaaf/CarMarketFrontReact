24e95061c9b177abd791d9e80e29be34dff43440
Zhenkaaf <zhenkaaf@gmail.com>
2024-02-29 12:29:47 +0200

    loginPage

---
+++ src/pages/loginPage/LoginPage.tsx
@@ -0,0 +1,177 @@
+import { useState } from "react";
+import { useForm, FieldValues } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { linkStyle } from "../../globalStyles";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Button,
+  TextField,
+  Container,
+  Typography,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+
+const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPass, setShowConfirmPass] = useState<boolean>(false);
+  const [phoneNumber, setPhoneNumber] = useState<string>("+380");
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+    watch,
+    setValue,
+  } = useForm({
+    mode: "onBlur",
+  });
+
+  const watchPassword = watch("password", "");
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(JSON.stringify(data));
+    reset();
+    setPhoneNumber("+380");
+  };
+
+  const excludeSpaces = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value.replace(/\s/g, "");
+    setValue(event.target.name, inputValue);
+  };
+
+  const togglePasswordsVisible = (field: string) => {
+    if (field === "password") {
+      setShowPassword(!showPassword);
+    } else if (field === "confirmPassword") {
+      setShowConfirmPass(!showConfirmPass);
+    }
+  };
+
+  const phoneNumberValidation = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    let inputValue = event.target.value;
+    inputValue = inputValue.replace(/[^0-9+]|(?<=\+.*?)[^0-9]/g, "");
+
+    if (inputValue.length < 4) {
+      inputValue = "+380";
+    }
+    if (inputValue.length > 13) {
+      inputValue = inputValue.slice(0, 13);
+    }
+    setPhoneNumber(inputValue);
+  };
+
+  return (
+    <Container
+      component="main"
+      maxWidth="xs"
+      style={{
+        boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.1)",
+        borderRadius: "8px",
+        paddingBottom: "20px",
+        paddingTop: "20px",
+        marginTop: "20px",
+      }}
+    >
+      <div>
+        <Typography
+          component="h1"
+          variant="h5"
+          align="center"
+          style={{
+            paddingBottom: "20px",
+          }}
+        >
+          LOGIN
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TextField
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: "Invalid email address",
+              },
+            })}
+            style={{ marginBottom: "20px" }}
+            required
+            fullWidth
+            label="Email"
+            name="email"
+            type="email"
+            onInput={excludeSpaces}
+            error={Boolean(errors?.email)}
+            helperText={
+              errors?.email && <p>{errors?.email?.message as string}</p>
+            }
+          />
+          <TextField
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Minimum 6 characters",
+              },
+              maxLength: {
+                value: 16,
+                message: "Maximum 16 characters",
+              },
+              pattern: {
+                value: /^[^\s]+$/,
+                message: "Spaces are not allowed",
+              },
+            })}
+            style={{ marginBottom: "20px" }}
+            required
+            fullWidth
+            label="Password"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            onInput={excludeSpaces}
+            error={Boolean(errors.password)}
+            helperText={
+              errors?.password && <p>{errors?.password?.message as string}</p>
+            }
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    onClick={() => togglePasswordsVisible("password")}
+                    edge="end"
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button
+            style={{ marginTop: "10px", marginBottom: "20px" }}
+            type="submit"
+            disabled={!isValid}
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Login
+          </Button>
+          <Typography align="center">
+            Do not have an account?
+            <Link
+              to="/registration"
+              style={linkStyle}
+            >
+              {" "}
+              Registration
+            </Link>
+          </Typography>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default LoginPage;
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Layout";
 import RegistrationPage from "../pages/registerPage/RegisterPage";
 import HomePage from "../pages/homePage/HomePage";
+import LoginPage from "../pages/loginPage/LoginPage";
 
 export const router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
       {
         path: "registration",
         element: <RegistrationPage />,
+      },
+      {
+        path: "login",
+        element: <LoginPage />,
       },
     ],
   },
