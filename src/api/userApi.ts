20eb65ba0e6696b94e1a418cf7786708b38410b0
Zhenkaaf <zhenkaaf@gmail.com>
2024-05-27 14:01:50 +0300

    newLinkToprojectApi

--- src/api/userApi.ts
+++ src/api/userApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromLocalStorage } from "../helpers/localStorage.helper";
 
 export const instance = axios.create({
-  baseURL: "https://excited-pantyhose-fox.cyclic.app/api/",
+  baseURL: "https://carmarketbacknest.onrender.com/api/",
   headers: {
     Authorization: `Bearer ` + getTokenFromLocalStorage(),
   },
